a39e09e8335586f0aa46f29e69cddf9a0b6c535a
Sushanth N <sushanthnandeti96@gmail.com>
2024-02-25 03:15:57 -0500

    configure Routes and State files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { Home } from '@mui/icons-material';
 import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import Homepage from 'scenes/homepage';
+import Loginpage from 'scenes/loginpage';
+import Profilepage from 'scenes/profilepage';
 
 function app() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+
+            <Route path = "/" element={<Loginpage />} />
+            <Route path = "/home" element={<Homepage />} />
+            <Route path = "/profile/:userId" element={<Profilepage />} />
+
+        </Routes>
+      </BrowserRouter>
       
     </div>
   );
---
+++ src/scenes/profilepage/index.jsx
@@ -0,0 +1,5 @@
+const Profilepage = () => {
+    return <div> Profilepage</div>
+};
+
+export default Profilepage;
